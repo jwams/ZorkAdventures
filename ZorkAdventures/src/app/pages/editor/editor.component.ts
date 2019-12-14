1edc7d0c013f22636d9839370460a2b91fe22de9
jwams <justin.wams@csc-scc.gc.ca>
2019-12-13 19:35:40 -0800

    Added ngx-bootstrap and dependancies

--- ZorkAdventures/src/app/pages/editor/editor.component.ts
+++ ZorkAdventures/src/app/pages/editor/editor.component.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Map } from '../../../Models/Map';
-//import * from '../../../Models/Map';
-//import Map = require('../../../Models/Map');
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
+
 export class EditorComponent implements OnInit {
 	
 	map: Map = new Map(10);
-	
-	index: Number = 1;
-	check: boolean = false;
-	
+
 	constructor() {
 		console.log(this.map.mapLayout[9][9]);
 	}
